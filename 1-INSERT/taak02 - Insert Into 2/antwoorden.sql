f95f38b0de2e38e0dc8780e6b482893910f191be
armankokor <2158154@talnet.nl>
2022-01-21 15:10:11 +0100

    OPD2

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (`klas`, `adres`, `postcode`) 
+VALUES ('9A', 'straat 1', '1234AE');
 
+INSERT INTO studenten (`klas`, `adres`, `postcode`) 
+VALUES ('9B', 'straat 2', '1234AA');
+
+INSERT INTO studenten (`klas`, `adres`, `postcode`) 
+VALUES ('9C', 'straat 3', '1234AL');
