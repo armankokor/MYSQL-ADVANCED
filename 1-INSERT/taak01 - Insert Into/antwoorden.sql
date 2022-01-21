4037a529ebea7149a61b48c3859ef5c770463257
armankokor <2158154@talnet.nl>
2022-01-21 14:53:04 +0100

    OPD1

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,18 @@
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (8, 'henk', 'van laag', '9A', 'straat 1', '1234AL', '2010-06-', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (9, 'jantje', 'groot', '9V', 'straat 2', '1234AA', '2010-06-05', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (11, 'peter', 'lang', '9P', 'straat 3', '1234AG', '2010-06-11', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (12, 'jan', 'zand', '9Q', 'straat 4', '1234AP', '2010-06-02', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (13, 'daan', 'van groot', '9V', 'straat 5', '1234AG', '2010-06-04', 'Amsterdam');
+
+INSERT INTO studenten (`student_id`, `voornaam`, `achternaam`, `klas`, `adres`, `postcode`, `geboortedatum`, `woonplaats`) 
+VALUES (14, 'don', 'van linden', '9B', 'straat 6', '1234AB', '2010-06-10', 'Amsterdam');
 
